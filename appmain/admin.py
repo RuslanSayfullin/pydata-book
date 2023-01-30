a2ee92080e4989a7ab5b03bd4c8add79672a6233
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-01-30 11:20:58 +0500

    Добавили первую форму.

--- appmain/admin.py
+++ appmain/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from appmain.models import Reckoning
+
+
+class ReckoningAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'designer', 'client_data', 'phone')    # Поля, которые отображаются в панели администратора
+    list_display_links = ('id', 'client_data')   # Поля, которые открывают заявку в панели администратора
+    search_fields = ('id', 'uuid',)     # Поля, по которым можно выполнять пойск
+
+
+admin.site.register(Reckoning, ReckoningAdmin)
