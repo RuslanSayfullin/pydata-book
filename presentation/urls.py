4ed6d11b3fece00dfcd5a3fff187e37dad280dea
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-02-22 11:21:33 +0500

    Изменили url

--- presentation/urls.py
+++ presentation/urls.py
@@ -6,7 +6,8 @@ from presentation.views.specification import specification_main_view
 app_name = 'presentation'
 urlpatterns = [
     re_path(r'^(?P<reckoning_uuid>\w+)/offerpages$', views.createupdateofferpage, name="create_update"),
-    re_path(r'^(?P<reckoning_uuid>\w+)/add_kitchen_offer_page$', views.addkitchenofferpage, name="addkitchenofferpage"),
+
+    re_path(r'^(?P<reckoning_uuid>\w+)/add_kitchen_offer_page$', views.addkitchenofferpage, name="add_kitchen_offer_page"),
     re_path(r'^(?P<reckoning_uuid>\w+)/kitchen_offerpages_list$', views.kitchen_offerpages_list, name='kitchen_offerpages_list'),
 
     re_path(r'^(?P<reckoning_uuid>\w+)/total_chek$', views.total_chek, name="total_chek"),
